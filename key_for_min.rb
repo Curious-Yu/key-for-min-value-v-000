6866f8f355401be0f6409e17175f6df3923bc58a
Wen Yu <wyu10amy@hotmail.com>
2021-06-07 06:17:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |name, num|
-    name
+  name_hash.collect do |name, value|
+
   end
+  name_hash
 end
